8288804d8bf455a1859c1d5ab7a1002624fcb95d
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-04-18 17:46:37 +0300

    Set code style.

--- src/fi/iki/dezgeg/matkakorttiwidget/MainMenuActivity.java
+++ src/fi/iki/dezgeg/matkakorttiwidget/MainMenuActivity.java
@@ -4,20 +4,22 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainMenuActivity extends Activity {
+public class MainMenuActivity extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
-    
+
 }
