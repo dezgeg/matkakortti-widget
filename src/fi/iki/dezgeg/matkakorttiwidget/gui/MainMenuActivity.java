70b9dd744ca670151906768a1624eb0ce0351cd7
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-10-26 16:38:52 +0300

    Start as widget configurer.

--- src/fi/iki/dezgeg/matkakorttiwidget/gui/MainMenuActivity.java
+++ src/fi/iki/dezgeg/matkakorttiwidget/gui/MainMenuActivity.java
@@ -1,22 +1,30 @@
 package fi.iki.dezgeg.matkakorttiwidget.gui;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.*;
-import android.content.*;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
 import android.text.Html;
-import android.view.Menu;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.List;
 
 import fi.iki.dezgeg.matkakorttiwidget.R;
 import fi.iki.dezgeg.matkakorttiwidget.matkakortti.Card;
-import fi.iki.dezgeg.matkakorttiwidget.matkakortti.MatkakorttiApi;
 
 public class MainMenuActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+    private int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     private List<Card> fetchedCards;
     private static final String[] PREF_KEYS = new String[]{"username", "password"};
 
@@ -66,25 +74,19 @@ public class MainMenuActivity extends PreferenceActivity implements OnSharedPref
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        requestWindowFeature(Window.FEATURE_PROGRESS);
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.main_menu);
-        getCardList().removeAll();
-    }
-
     private PreferenceGroup getCardList() {
         return (PreferenceGroup) findPreference("cardList");
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-        for (String prefKey : PREF_KEYS)
-            updatePrefTitle(getPreferenceScreen().getSharedPreferences(), prefKey);
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.main_menu);
+        setContentView(R.layout.settings_menu);
+
         findPreference("registerLink").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
@@ -94,6 +96,29 @@ public class MainMenuActivity extends PreferenceActivity implements OnSharedPref
                 return true;
             }
         });
+        ((ImageButton)findViewById(R.id.settings_ok_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultValue = new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+                MainMenuActivity.this.setResult(RESULT_OK, resultValue);
+                MainMenuActivity.this.finish();
+            }
+        });
+
+        setResult(RESULT_CANCELED);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+            appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        for (String prefKey : PREF_KEYS)
+            updatePrefTitle(getPreferenceScreen().getSharedPreferences(), prefKey);
         updateCardList();
     }
 
@@ -106,10 +131,12 @@ public class MainMenuActivity extends PreferenceActivity implements OnSharedPref
     @Override
     public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
         updatePrefTitle(prefs, key);
-        if (key.equals("username") || key.equals("password")) {
-            if (!findPreference("username").equals("") && !findPreference("password").equals("")) {
+
+        if (key.startsWith("cardSelected_") && fetchedCards != null) {
+            WidgetUpdaterService.updateWidgets(getApplicationContext(), fetchedCards);
+        } else if (key.equals("username") || key.equals("password")) {
+            if (!findPreference("username").equals("") && !findPreference("password").equals(""))
                 updateCardList();
-            }
         }
     }
 
@@ -118,9 +145,8 @@ public class MainMenuActivity extends PreferenceActivity implements OnSharedPref
     }
 
     private void updatePrefTitle(SharedPreferences prefs, String key) {
-        if (key.startsWith("cardSelected_") && fetchedCards != null) {
-            WidgetUpdaterService.updateWidgets(getApplicationContext(), fetchedCards);
-        } else if (key.equals("password")) {
+
+        if (key.equals("password")) {
             String password = prefs.getString(key, "");
             String masked = "";
             for (int i = 0; i < password.length(); i++)
