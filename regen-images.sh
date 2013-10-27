6910aa7b15c03fbd617bac9148a23ad30497845a
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-10-27 21:33:36 +0200

    Use image for widget icons.

--- regen-images.sh
+++ regen-images.sh
@@ -2,3 +2,10 @@
 cd $(dirname $0)/res/drawable
 
 convert ok.png -colorspace gray ok_gray.png
+
+for f in euro_sign watch; do
+    rm $f.png 2>/dev/null
+    # inkscape svg/$f.svg --export-png=$f.png --export-width=64
+    convert svg/$f.svg -background none -resize 128x $f.png
+    convert -transparent white +level-colors yellow, $f.png $f.png
+done
