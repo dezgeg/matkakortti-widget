2e12223e40e626cfc1cd78d4a7abe2a101b60c3d
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-11-02 23:55:28 +0200

    Add debug test code.

--- src/fi/iki/dezgeg/matkakorttiwidget/gui/MatkakorttiWidgetApp.java
+++ src/fi/iki/dezgeg/matkakorttiwidget/gui/MatkakorttiWidgetApp.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import fi.iki.dezgeg.matkakorttiwidget.matkakortti.Card;
@@ -11,6 +14,7 @@ import fi.iki.dezgeg.matkakorttiwidget.matkakortti.MatkakorttiApi;
 
 public class MatkakorttiWidgetApp extends Application {
     private static SharedPreferences prefs;
+    public static final boolean DEBUG = true;
 
     // Synchronized since when the app is started from the IDE,
     // the SettingsActivity and the widget updater run concurrently ->
@@ -19,6 +23,13 @@ public class MatkakorttiWidgetApp extends Application {
         String username = prefs.getString("username", "<not set>");
         String password = prefs.getString("password", "");
 
+        if (DEBUG && username.equals("dezgegt"))
+            return Arrays.asList(
+                    new Card("Tyhja", "a", BigDecimal.ZERO, null),
+                    new Card("Rahaa", "b", BigDecimal.TEN, null),
+                    new Card("Kautta", "c", BigDecimal.ZERO, new Date(113, 11, 28)),
+                    new Card("Raha+Kausi", "d", new BigDecimal("99.99"), new Date()),
+                    new Card("Paljon rahaa", "e", new BigDecimal("199.99"), null));
         return new MatkakorttiApi(username, password).getCards();
     }
 
